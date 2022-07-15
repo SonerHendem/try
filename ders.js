02bd8abf5f596d5a7ed0f19846e6cd65561a9f53
SonerHendem <sonerhendem@gmail.com>
2022-07-15 23:05:05 +0300

    diziler ile ilgili not ve çalışmalarım

---
+++ ders.js
@@ -0,0 +1,65 @@
+let names = ["Soner","Tugce","Duygu","muhammed","Nuran"];
+document.write(names+'<br />')
+
+//-----------------
+document.write('<hr />'+'<hr />')
+let fruits =['apple','pear','apricot','cherry'];
+let result = fruits;
+document.write(fruits +'<br />')
+document.write(result +'<br />')
+
+document.write(fruits[0] +'<br />')
+document.write(fruits[0] +'<br />')
+
+//---------------------------------------
+document.write('<hr />'+'<hr />')
+let colors =['black','blue','red',],names2=['soner','tugce','dugu'];
+document.write(colors +'<br />')
+document.write(colors[0] +'<br />')
+document.write(colors[1] +'<br />')
+document.write(colors[2] +'<br />')
+
+document.write(names2 +'<br />')
+document.write(names2[0] +'<br />')
+document.write(names2[1] +'<br />')
+document.write(names2[2] +'<br />')
+
+//---------------
+document.write('<hr />'+'<hr />')
+//const names3=  ['soner','ibo','nuran']; //const ile değişken oluşturulamaz!
+let names3=  ['soner','ibo','nuran']; //let ve var ile oluşturulur!
+document.write(names3 +'<br />')
+document.write(names3[0] +'<br />')
+document.write(names3[1] +'<br />')
+document.write(names3[2] +'<br />')
+//-----------------------------------------
+document.write('<hr />'+'<hr />')
+let ornek =['hakan','pelin','selin']
+document.write(names3 +'<br />')
+document.write(ornek[0] +'<br />')
+document.write(ornek[1] +'<br />')
+document.write(ornek[2] +'<br />')
+
+//----------------------------
+document.write('<hr />'+'<hr />')
+let deneme='javascript';
+document.write(deneme[0] +'<br />')
+document.write(deneme[1] +'<br />')
+document.write(deneme[2] +'<br />')
+document.write(deneme[3] +'<br />')
+document.write(deneme[4] +'<br />')
+document.write(deneme[5] +'<br />')
+document.write(deneme[6] +'<br />')
+document.write(deneme[7] +'<br />')
+document.write(deneme[8] +'<br />')
+document.write(deneme[9] +'<br />')
+
+
+
+
+
+
+
+
+
+
