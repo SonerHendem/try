dbe2d036cb2ae14760acb77957ecd2c4eae694af
SonerHendem <sonerhendem@gmail.com>
2022-07-30 18:14:34 +0300

    güncel

---
+++ fonksiyon/index.js
@@ -0,0 +1,46 @@
+document.write('<hr />'+'FONKSİYON DERSLERİ   '+'<hr />')
+
+
+//-------------------------------------
+document.write('<hr />' +'Fonksiyon() = FONKSİYONLARIN KAPSAMA ALANI! (fonksiyonlar dışardan paramatre alabilir.) '+'<hr />')
+let bir = 'soner ';
+let iki = 'hendem';
+function demo(){
+    document.write(bir + iki)
+}
+demo();
+
+
+document.write('<hr />' +'Fonksiyon() = FONKSİYONLARIN KAPSAMA ALANI! (fonksiyonlar fonksiyon içerisinden aldığı paramatreleri fonksiyon dışında çağıramaz.) '+'<hr />')
+
+// function demo1(){
+//     let birr = 'altunkoy ';
+//     let ikii = 'tuğge';
+// }
+// demo1();
+// document.write(birr + ikii +' boş çıktı.')
+document.write('fonksiyon çalışmadı.'+ '<br/>')
+document.write('değişkenler global bir alandan local biralana(fonksiyon) çağırılabilir..'+ '<br/>')
+
+
+
+//  document.write(names8 + '<br/>')
+
+
+
+
+
+
+
+
+
+
+// //-------------------------------------
+// document.write('<hr />' +'Fonksiyon() = FONKSİYONLARIN KAPSAMA ALANI! Tanımı =    '+'<hr />')
+
+// //  document.write(names8 + '<br/>')
+
+// //-------------------------------------
+// document.write('<hr />' +'Fonksiyon() = FONKSİYONLARIN KAPSAMA ALANI! Tanımı =    '+'<hr />')
+
+// //  document.write(names8 + '<br/>')
